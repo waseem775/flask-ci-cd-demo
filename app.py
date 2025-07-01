98c5ae6cba80eee60b3017058138825772e5c5fc
waseem775 <waseem.hassan@focusitservices.co.uk>
2025-07-01 20:30:40 +0100

    app is working perfectly

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask
+import os
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD with Docker!"
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("POSTGRES_HOST", "localhost"),
+            database=os.environ.get("POSTGRES_DB", "testdb"),
+            user=os.environ.get("POSTGRES_USER", "testuser"),
+            password=os.environ.get("POSTGRES_PASSWORD", "testpass"),
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        db_version = cur.fetchone()
+        cur.close()
+        conn.close()
+        return f"Connected to PostgreSQL: {db_version}"
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
